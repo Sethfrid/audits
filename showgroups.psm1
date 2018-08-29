b805d6187d429fd3d1bc0dafdf57a8ac0718efb7
Sethfrid <oblivion_zero@hotmail.com>
2018-08-29 08:27:21 +0100

    Add files via upload
    A function for showing all groups in AD and another function for showing all active users and their groups they are member of.

---
+++ showgroups.psm1
@@ -0,0 +1,19 @@
+﻿function show-groups {
+
+$GroupList = Get-ADGroup -Filter * -Properties Name, DistinguishedName, GroupCategory, GroupScope, whenCreated, whenChanged, member, memberOf, SamAccountName, Description |            
+    Select-Object Name, DistinguishedName, GroupCategory, GroupScope, whenCreated, whenChanged, member, memberOf, SamAccountName, Description, 
+        @{name='MemberCount';expression={$_.member.count}}, `
+        @{name='MemberOfCount';expression={$_.memberOf.count}}, `
+        @{name='DaysSinceChange';expression=`
+            {[math]::Round((New-TimeSpan $_.whenChanged).TotalDays,0)}} | Sort-Object Name            
+            
+$GroupList | Select Name, SamAccountName, Description, DistinguishedName, GroupCategory, GroupScope, whenCreated, whenChanged, DaysSinceChange, membercount, memberofcount | Export-CSV c:\temp\AllGroups.csv -NoTypeInformation
+
+}
+
+
+function user-groups {
+
+Get-ADUser -Filter {enabled -eq $true} -Properties memberof |select name, @{L='Groups'; E={$_.memberof}} | Export-Csv -Path c:\temp\usersgroups.csv -NoTypeInformation
+
+}
